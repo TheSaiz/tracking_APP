dee5d003e212538ed4bd6c1256022a5fcd1679ef
TheSaiz <86813512+TheSaiz@users.noreply.github.com>
2025-11-24 10:26:06 -0300

    Create Build.Gradle.kts

---
+++ Build.Gradle.kts
@@ -0,0 +1,74 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.tracker.tracking"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.tracker.tracking"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    dependencies {
+        implementation(libs.androidx.core.ktx)
+        implementation(libs.androidx.lifecycle.runtime.ktx)
+        implementation(libs.androidx.activity.compose)
+        implementation(platform(libs.androidx.compose.bom))
+        implementation(libs.androidx.ui)
+        implementation(libs.androidx.ui.graphics)
+        implementation(libs.androidx.ui.tooling.preview)
+        implementation(libs.androidx.material3)
+        testImplementation(libs.junit)
+        androidTestImplementation(libs.androidx.junit)
+        androidTestImplementation(libs.androidx.espresso.core)
+        androidTestImplementation(platform(libs.androidx.compose.bom))
+        androidTestImplementation(libs.androidx.ui.test.junit4)
+        debugImplementation(libs.androidx.ui.tooling)
+        debugImplementation(libs.androidx.ui.test.manifest)
+        implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+        implementation("com.squareup.okhttp3:okhttp:4.12.0") // Para la red
+        implementation("com.google.android.gms:play-services-location:21.0.1")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
+        implementation("androidx.compose.material:material-icons-extended:1.6.8")
+        implementation("com.google.zxing:core:3.5.3")
+        implementation("androidx.camera:camera-core:1.3.1")
+        implementation("androidx.camera:camera-camera2:1.3.1")
+        implementation("androidx.camera:camera-lifecycle:1.3.1")
+        implementation("androidx.camera:camera-view:1.3.1") // Para PreviewView
+        implementation("androidx.camera:camera-extensions:1.3.1")
+    }
+
+
+}
